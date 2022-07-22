1e16c7f7ce1f32d10e54fb7663bb5024b6b22d47
PhiNgo <phingo36@gmail.com>
2022-07-22 22:05:28 +0700

    Install Victoria

---
+++ Yoimiya Bot/Modules/Commands.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yoimiya_Bot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        //Music
+        //[Command("Join")]
+        //public async Task Join()
+        //{
+
+        //}
+    }
+}
---
+++ Yoimiya Bot/Services/MusicService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Victoria;
+
+namespace Yoimiya_Bot.Services
+{
+    public class MusicService
+    {
+        private LavaRestClient _lavaRestClient;
+    }
+}
