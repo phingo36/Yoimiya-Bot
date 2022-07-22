d564b295334ee65a7edf5e8a77c454a0c0db7a4c
phingo36 <55975870+phingo36@users.noreply.github.com>
2022-07-22 22:07:19 +0700

    Install Victoria (#1)

--- Yoimiya Bot/Program.cs
+++ Yoimiya Bot/Program.cs
@@ -1,15 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Yoimiya_Bot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+        private DiscordSocketClient _client;
+        private CommandService _command;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
         {
+            _client = new DiscordSocketClient();
+            _command = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_command)
+                .BuildServiceProvider();
+            string token = "OTk5OTEyNzU0NTI3ODc5MTg4.GRVEjH.oqRRIqEW0AoR5PZTpgY8TGxEMcLDkeyfVY99Sw";
+
+            _client.Log += _client_Log;
+
+            await RegisterCommandsAsync();
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _command.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("!", ref argPos))
+            {
+                var result = await _command.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.Error);
+            }
         }
     }
 }
