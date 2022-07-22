6f6f47eb06c5bc279a5f92426e36a0596e8b4bb6
Bongya <Bongya@BONGYA>
2022-07-22 12:50:46 +0700

    Add project files.

---
+++ Yoimiya Bot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yoimiya_Bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
